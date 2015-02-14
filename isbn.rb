6f3b4f5b0402c4fef5658d51fb46d71e2c1f1069
johnsorrentino <sorrentino.john@gmail.com>
2015-02-14 14:17:41 -0600

    evrything should work

--- isbn.rb
+++ isbn.rb
@@ -1,22 +1,22 @@
-class Isbn
-  def self.clean_isbn(isbn)
-    isbn = isbn.gsub(/[^0-9xX]/, '')
-    isbn = isbn.gsub(/x/, 'X')
-    isbn = isbn.gsub(/-/, '')
-  end
+module ISBN
 
   # http://stackoverflow.com/a/14096142/3224822
-  # http://stackoverflow.com/a/519593/3224822
-  def self.extract_isbn(isbn)
-    isbn[/\b(?:ISBN(?:: ?| ))?((?:97[89])?\d{9}[\dx])\b/i]
+  def self.parse(isbn)
+    isbn = isbn.gsub(/x/, 'X')                                    # Use capital X
+    isbn = isbn.gsub(/[^0-9X]/, '')                               # Whitelist characters
+    isbn = isbn[/\b(?:ISBN(?:: ?| ))?((?:97[89])?\d{9}[\dx])\b/i] # Extract
+    return isbn if valid?(isbn)
   end
 
-  def self.valid_isbn_10?(isbn)
-    isbn[9] == calculate_isbn_10_check_digit(isbn)
-  end
-
-  def self.valid_isbn_13?(isbn)
-    isbn[12] == calculate_isbn_13_check_digit(isbn)
+  def self.valid?(isbn)
+    return false if isbn.nil?
+    if isbn.length == 10
+      isbn[9] == calculate_isbn_10_check_digit(isbn)
+    elsif isbn.length == 13
+      isbn[12] == calculate_isbn_13_check_digit(isbn)
+    else
+      false
+    end
   end
 
   def self.convert_isbn_10_to_isbn_13(isbn)
@@ -54,4 +54,5 @@ class Isbn
     end
     sum
   end
+
 end
