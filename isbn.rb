3168ef06c09d09680db4efa029fc890dac4b0190
johnsorrentino <sorrentino.john@gmail.com>
2015-02-15 16:01:23 -0600

    improvements? ;

--- isbn.rb
+++ isbn.rb
@@ -1,16 +1,36 @@
 module ISBN
 
+  def self.parse(isbn, type = nil)
+    isbn = clean(isbn)
+    if type == :isbn_10
+      return isbn if valid?(isbn, :isbn_10)
+    elsif type == :isbn_13
+      return isbn if valid?(isbn, :isbn_13)
+    elsif isbn.length == 10
+      return isbn if valid?(isbn, :isbn_10)
+    elsif isbn.length == 13
+      return isbn if valid?(isbn, :isbn_13)
+    else
+      nil
+    end
+  end
+
   # http://stackoverflow.com/a/14096142/3224822
-  def self.parse(isbn)
+  def self.clean(isbn)
     isbn = isbn.gsub(/x/, 'X')                                    # Use capital X
     isbn = isbn.gsub(/[^0-9X]/, '')                               # Whitelist characters
     isbn = isbn[/\b(?:ISBN(?:: ?| ))?((?:97[89])?\d{9}[\dx])\b/i] # Extract
-    return isbn if valid?(isbn)
   end
 
-  def self.valid?(isbn)
+  def self.valid?(isbn, type = nil)
     return false if isbn.nil?
-    if isbn.length == 10
+    if type == :isbn_10
+      return false if isbn.length != 10
+      isbn[9] == calculate_isbn_10_check_digit(isbn)
+    elsif type == :isbn_13
+      return false if isbn.length != 13
+      isbn[12] == calculate_isbn_13_check_digit(isbn)
+    elsif isbn.length == 10
       isbn[9] == calculate_isbn_10_check_digit(isbn)
     elsif isbn.length == 13
       isbn[12] == calculate_isbn_13_check_digit(isbn)
@@ -38,7 +58,7 @@ module ISBN
   def self.calculate_isbn_13_check_digit(isbn)
     array_1 = [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3]
     array_2 = isbn[0..11].split('').map(&:to_i)
-    check_digit = 10 - (sum_product(array_1, array_2) % 10)
+    check_digit = (10 - (sum_product(array_1, array_2) % 10)) % 10
     check_digit.to_s
   end
 
